d12a38aebf57f8abbb64ff342382f4d4dfa45d03
Thucnv <nguyenvanthuc1912@gmail.com>
2020-07-08 10:39:27 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 GIT API URL: https://github.com/itimius94/qltc-api.git
+URL_ADMIN: https://qltc-canhan.herokuapp.com/
--- src/configs/index.js
+++ src/configs/index.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://qltc-api.herokuapp.com/'
+export const API_URL = 'https://qltc-api.herokuapp.com'
